a75f0491ae4958976a594908b5cd11a2ba8e2a46
Jacob Brewer <jacobbrewerrooney@gmail.com>
2023-10-10 15:17:13 -0400

    v2.1

--- src/components/Marquee.tsx
+++ src/components/Marquee.tsx
@@ -1,35 +1,31 @@
 "use client"
+
 import { useRef } from "react";
-import {
-  motion,
-  useScroll,
-  useSpring,
-  useTransform,
-  useMotionValue,
-  useVelocity,
-  useAnimationFrame,
-} from "framer-motion";
 import { wrap } from "@motionone/utils";
+import { motion, useScroll, useSpring, useTransform, useMotionValue, useVelocity, useAnimationFrame} from "framer-motion";
 
 interface ParallaxProps {
   children: React.ReactNode;
   baseVelocity: number;
+  className?: string;
+  array?: number;
 }
 
-const Marquee = ({ children, baseVelocity = 100 }: ParallaxProps) => {
+const Marquee = ({ children, baseVelocity = 100, className, array }: ParallaxProps) => {
     const baseX = useMotionValue(0);
     const { scrollY } = useScroll();
     const scrollVelocity = useVelocity(scrollY);
+    const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
+
     const smoothVelocity = useSpring(scrollVelocity, {
       damping: 50,
       stiffness: 400,
     });
+    
     const velocityFactor = useTransform(smoothVelocity, [0, 1000], [0, 5], {
       clamp: false,
     });
-  
-    const x = useTransform(baseX, (v) => `${wrap(-20, -45, v)}%`);
-  
+    
     const directionFactor = useRef<number>(1);
     useAnimationFrame((t, delta) => {
       let moveBy = directionFactor.current * baseVelocity * (delta / 1000);
@@ -47,11 +43,12 @@ const Marquee = ({ children, baseVelocity = 100 }: ParallaxProps) => {
   
     return (
       <div className="overflow-hidden m-0 whitespace-nowrap flex flex-nowrap">
-        <motion.div className="flex whitespace-nowrap flex-nowrap bg-white text-black" style={{ x }}>
-          <span className="block mr-5">{children}</span>
-          <span className="block mr-5">{children}</span>
-          <span className="block mr-5">{children}</span>
-          <span className="block mr-5">{children}</span>
+        <motion.div className={`flex whitespace-nowrap flex-nowrap ${className}`} style={{ x }}>
+            {Array(array || 4).fill(null).map((_, index) => (
+                <span key={index} className="block mr-8">
+                    {children}
+                </span>
+            ))}
         </motion.div>
       </div>
     );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,154 +1,41 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from 'tailwindcss'
 const plugin = require('tailwindcss/plugin')
 
-module.exports = {
-
-  // Customize content if styles are not being applied https://tailwindcss.com/docs/content-configuration
+const config: Config = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  // Customize the default Tailwind CSS theme https://tailwindcss.com/docs/theme
   theme: {
     extend: {
-      // Add brand colors here https://tailwindcss.com/docs/customizing-colors
-      colors: {
-        'primary': '#4287f5',
-        'secondary': '#f5b642',
-        'tertiary': '#f54242',
-        'quaternary': '#42f560',
-        'black': '#000000',
-        'white': '#FFFFFF',
-        'gray': '#F2F2F2',
-        'gray-800': '#1F2937',
-        'gray-300': '#D1D5DB',
-      },
-      // Add fonts here https://tailwindcss.com/docs/font-family
-      fontFamily: {
-        heading: ['arial', 'sans-serif'],
-        sans: ['Montserrat']
-      },
-      lineHeight: {
-        default: "1.7"
-      },
-      // Customize font styles here https://tailwindcss.com/docs/font-size
-      // Customize default head tag styles './app/globals.css'
-      fontSize: {
-        'h1': [
-          '4rem', {
-            lineHeight: '82px',
-            fontWeight: '700',
-            fontFamily: 'Bebas',
-          }
-        ],
-        'h1-alt': [
-          '4rem', {
-            lineHeight: '82px',
-            fontWeight: '400',
-            fontFamily: 'Bebas',
-          }
-        ],
-        'h2': [
-          '3rem', {
-            lineHeight: '61px',
-            fontWeight: '700',
-            fontFamily: 'Bebas',
-          }
-        ],
-        'h2-alt': [
-          '3rem', {
-            lineHeight: '61px',
-            fontWeight: '400',
-            fontFamily: 'Bebas',
-          }
-        ],
-        'h3': [
-          '2rem', {
-            lineHeight: '41px',
-            fontWeight: '700',
-            fontFamily: 'Bebas',
-          }
-        ],
-        'body': [
-          '1.1rem', {
-            lineHeight: '31px',
-            fontWeight: '400',
-            fontFamily: 'Montserrat',
-          }
-        ],
-        'body-alt': [
-          '1rem', {
-            lineHeight: '31px',
-            fontWeight: '500',
-            fontFamily: 'Bebas',
-          }
-        ],
-      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      //
-      //
-      // Customize widths and heights as they apear in Figma
-      minWidth: {
-        'full': '100%',
-        'half': '50%',
-        'quarter': '25%',
-      },
-      maxWidth: {
-        'half': '50%',
-        'quarter': '25%',
-        '50': '50px',
-        '100': '100px',
-        '150': '150px',
-      },
-      minHeight: {
-        'full': '100%',
-        'half': '50%',
-        'quarter': '25%',
-        '100vw': '100vw',
-        '100vh': '100vh',
-        '50': '50px',
-      },
+      colors: {
+        'primary': '#eb4034',
+        'secondary': '#e89b27',
+        'dark': '#171514',
+        'light': '#f7f4f0'
+      }
     },
   },
   plugins: [
     plugin(function({ addComponents }:any) {
-      // Add your custom styles here
       addComponents({
-        '.btn-primary': {
-          padding: '.5rem 1rem',
-          borderRadius: '.25rem',
-          backgroundColor: '#4287f5',
-          fontWeight: '600',
-          '&:hover': {
-            opacity: '.8'
-          },
-        },
-        '.btn-secondary': {
-          backgroundColor: '#f5b642',
-          color: '#fff',
+        '.btn': { // Example with breakpoints
           padding: '.5rem 1rem',
           borderRadius: '.25rem',
           fontWeight: '600',
-          '&:hover': {
-            opacity: '.8'
+          backgroundColor: 'red',
+          '@screen sm': {
+            backgroundColor: 'blue',
           },
-        },
-        '.btn-tertiary': {
-          backgroundColor: '#e3342f',
-          color: '#fff',
-          padding: '.5rem 1rem',
-          borderRadius: '.25rem',
-          fontWeight: '600',
-          '&:hover': {
-            opacity: '.8'
-          },
-        },
+        }
       })
     })
-  ],
+  ]
 }
+export default config
