3410cf2f90710f3b7dcbbf7621a135b506542f5b
Jacob Brewer <jacobbrewerrooney@gmail.com>
2024-04-17 16:31:49 -0400

    updates from steele

--- sanity/env.ts
+++ sanity/env.ts
@@ -7,7 +7,7 @@ export const dataset = assertValue(
 )
 
 export const projectId = assertValue(
-  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'cf9d3z4o',
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'zxomdnuv',
   'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
 )
 
